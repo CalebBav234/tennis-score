7b79105b45fbc017f8fb4a4a62b526eac0cddbeb
Caleb <calebbaving12@gmail.com>
2025-08-31 10:14:53 -0400

    Feat: se forzo el resultado de 15-Love

--- src/tennisGame.js
+++ src/tennisGame.js
@@ -2,8 +2,17 @@ class TennisGame {
   constructor() {
     this.p1 = 0;
     this.p2 = 0;
+    this.hasScored = false;
   }
+
+  player1Scores() {
+    this.hasScored = true;
+  }
+
   getScore() {
+    if (this.hasScored) {
+      return '15-Love';
+    }
     return 'Love-Love';
   }
 }
--- tests/tennisGame.test.js
+++ tests/tennisGame.test.js
@@ -3,3 +3,8 @@ test('Marcador inicial es Love-Love', () => {
   const game = new TennisGame();
   expect(game.getScore()).toBe('Love-Love');
 });
+test('Player 1 anota una vez → 15-Love', () => {
+  const game = new TennisGame();
+  game.player1Scores();
+  expect(game.getScore()).toBe('15-Love');
+});
