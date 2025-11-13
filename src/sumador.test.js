b0f7a336c53ec21878b365ee1e840ad2ccc507a2
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2025-11-13 00:07:37 -0400

    Prueba TDD log2

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -5,7 +5,7 @@ describe('Sumar', () => {
     expect(sumar(3, 2)).toEqual(5);
   });
 
-  it('debería lanzar error si los parámetros no son numéricos', () => {
+  it('debería lanzar error si los parámetros no son numéricos', () => { 
     expect(() => sumar('3', '2')).toThrow('Los parámetros deben ser numéricos');
   });
 });
